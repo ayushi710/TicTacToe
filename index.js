2ccf809fda10c4ed5879edb68c5442e767522eb2
ayushi <ayushimittal710@gmail.com>
2018-05-06 02:38:16 +0530

    done

---
+++ index.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+    var mark ='X';
+    var count = 0;
+    $('.left').css({"background-color":"#26A69A" , "color" : "white"});
+    console.log('ready');
+    $('.block').click(function () {
+        if(($(this).text() === '')&&(mark ==='X')) {
+            mark ='0';
+            $(this).text('0');
+            count++ ;
+            $('.right').css({"background-color":"#26A69A" , "color" : "white"});
+            $('.left').css({"background-color":"white" , "color" : "#26A69A"});
+        }
+        else if(($(this).text() === '')&&(mark ==='0')) {
+            mark = 'X';
+            $(this).text('X');
+            count++ ;
+            $('.left').css({"background-color":"#26A69A" , "color" : "white"});
+            $('.right').css({"background-color":"white" , "color" : "#26A69A"});
+        }
+    });
+    function checkPlayer( m ) {
+        if(m === '0') {
+            return 1;
+        }
+        else if(m === 'X') {
+            return 2;
+        }
+
+    }
+    function disable() {
+        $('.block').css({'pointer-events':'none','background-color' :'rgba(38, 166, 154,0.5)'});
+    }
+    setInterval( function () {
+        var b = document.getElementsByClassName('block');
+        var playerNo;
+        if((b[0].innerHTML=== b[1].innerHTML) &&(b[1].innerHTML === b[2].innerHTML)) {
+            playerNo = checkPlayer(b[0].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable();
+            }
+        }
+        else if((b[3].innerHTML=== b[4].innerHTML) &&(b[4].innerHTML === b[5].innerHTML)) {
+            playerNo = checkPlayer(b[3].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[6].innerHTML=== b[7].innerHTML) &&(b[7].innerHTML === b[8].innerHTML)) {
+            playerNo = checkPlayer(b[6].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[0].innerHTML=== b[3].innerHTML) &&(b[3].innerHTML === b[6].innerHTML)) {
+            playerNo = checkPlayer(b[3].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[1].innerHTML=== b[4].innerHTML) &&(b[4].innerHTML === b[7].innerHTML)) {
+            playerNo = checkPlayer(b[1].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[2].innerHTML=== b[5].innerHTML) &&(b[8].innerHTML === b[5].innerHTML)) {
+            playerNo = checkPlayer(b[2].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[0].innerHTML=== b[4].innerHTML) &&(b[4].innerHTML === b[8].innerHTML)) {
+            playerNo = checkPlayer(b[0].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if((b[2].innerHTML=== b[4].innerHTML) &&(b[4].innerHTML === b[6].innerHTML)) {
+            playerNo = checkPlayer(b[2].innerHTML);
+            if(playerNo !== undefined) {
+                $('h1').text('Player '+ playerNo +' Wins !');
+                disable()
+            }
+        }
+        else if(count===9) {
+            $('h1').text('Draw !');
+            disable()
+        }
+
+    } , 500);
+
+    $('.restart').click(function () {
+        var b = document.getElementsByClassName('block');
+        var i;
+        for(i=0;i<9;i++) {
+            b[i].innerHTML= '';
+        }
+        $('h1').text('');
+        $('.block').css({'pointer-events':'auto','background-color' :'rgba(38, 166, 154,1)'});
+    });
+
+});
